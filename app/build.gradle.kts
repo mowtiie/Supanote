7f25f00554a7effdf5542aecd4242e4e0c47c1d7
mowtiie <mowtiie.dev@gmail.com>
2026-06-29 02:02:17 +0800

    Added okhttp dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.okhttp)
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
     implementation(libs.constraintlayout)
