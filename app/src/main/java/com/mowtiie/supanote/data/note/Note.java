b0b37aeff1d3dee60ada8674850c5c5001f82978
mowtiie <mowtiie.dev@gmail.com>
2026-06-29 02:05:13 +0800

    Created note model class

---
+++ app/src/main/java/com/mowtiie/supanote/data/note/Note.java
@@ -0,0 +1,52 @@
+package com.mowtiie.supanote.data.note;
+
+public class Note {
+
+    private long id;
+    private String title;
+    private String content;
+    private String createdAt;
+
+    public Note() {
+        // Default constructor
+    }
+
+    public Note(long id, String title, String content, String createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
