ca741347b79453d3837c0718f3846e3cc3857b68
mowtiie <mowtiie.dev@gmail.com>
2026-06-29 19:28:54 +0800

    Created connection manager instance in app class

--- app/src/main/java/com/mowtiie/supanote/SupanoteApp.java
+++ app/src/main/java/com/mowtiie/supanote/SupanoteApp.java
@@ -2,19 +2,26 @@ package com.mowtiie.supanote;
 
 import android.app.Application;
 
+import com.mowtiie.supanote.data.local.ConnectionManager;
 import com.mowtiie.supanote.data.local.SessionManager;
 
 public class SupanoteApp extends Application {
 
     private SessionManager session;
+    private ConnectionManager connection;
 
     @Override
     public void onCreate() {
         super.onCreate();
         session = new SessionManager(this);
+        connection = new ConnectionManager(this);
     }
 
     public SessionManager session() {
         return session;
+    }
+
+    public ConnectionManager connection() {
+        return connection;
     }
 }
