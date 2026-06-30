0ac10bd76c417024019fa8804de644fabfc4226a
mowtiie <mowtiie.dev@gmail.com>
2026-06-30 23:51:55 +0800

    Updated note model class

--- app/src/main/java/com/mowtiie/supanote/data/model/Note.java
+++ app/src/main/java/com/mowtiie/supanote/data/model/Note.java
@@ -1,29 +1,63 @@
 package com.mowtiie.supanote.data.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Note {
 
-    private long id;
+    private String id;
+    private String userId;
+    private String folderId;
     private String title;
     private String content;
+    private boolean isArchived;
+    private boolean isPinned;
     private String createdAt;
+    private String updatedAt;
+    private String deletedAt;
+
+    private List<Tag> tags = new ArrayList<>();
 
     public Note() {
         // Default constructor
     }
 
-    public Note(long id, String title, String content, String createdAt) {
+    public Note(String id, String userId, String folderId, String title, String content, boolean isArchived, boolean isPinned, String createdAt, String updatedAt, String deletedAt, List<Tag> tags) {
         this.id = id;
+        this.userId = userId;
+        this.folderId = folderId;
         this.title = title;
         this.content = content;
+        this.isArchived = isArchived;
+        this.isPinned = isPinned;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.tags = tags;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFolderId() {
+        return folderId;
+    }
+
+    public void setFolderId(String folderId) {
+        this.folderId = folderId;
     }
 
     public String getTitle() {
@@ -42,6 +76,22 @@ public class Note {
         this.content = content;
     }
 
+    public boolean isArchived() {
+        return isArchived;
+    }
+
+    public void setArchived(boolean archived) {
+        isArchived = archived;
+    }
+
+    public boolean isPinned() {
+        return isPinned;
+    }
+
+    public void setPinned(boolean pinned) {
+        isPinned = pinned;
+    }
+
     public String getCreatedAt() {
         return createdAt;
     }
@@ -49,4 +99,33 @@ public class Note {
     public void setCreatedAt(String createdAt) {
         this.createdAt = createdAt;
     }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(String deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public boolean isInTrash() {
+        return deletedAt != null;
+    }
+
 }
