62c0357b31a4b7697365cd5e860d60b48d94a0e1
mowtiie <mowtiie.dev@gmail.com>
2026-07-05 23:26:42 +0800

    Added main and auth http client for app class

--- app/src/main/java/com/mowtiie/supanote/SupanoteApp.java
+++ app/src/main/java/com/mowtiie/supanote/SupanoteApp.java
@@ -4,17 +4,26 @@ import android.app.Application;
 
 import com.mowtiie.supanote.data.local.ConnectionManager;
 import com.mowtiie.supanote.data.local.SessionManager;
+import com.mowtiie.supanote.data.remote.TokenAuthenticator;
+
+import okhttp3.OkHttpClient;
 
 public class SupanoteApp extends Application {
 
     private SessionManager session;
     private ConnectionManager connection;
+    private OkHttpClient httpClient;
+    private OkHttpClient authedHttpClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
         session = new SessionManager(this);
         connection = new ConnectionManager(this);
+        httpClient = new OkHttpClient();
+        authedHttpClient = httpClient.newBuilder()
+                .authenticator(new TokenAuthenticator(connection, session))
+                .build();
     }
 
     public SessionManager session() {
@@ -23,5 +32,13 @@ public class SupanoteApp extends Application {
 
     public ConnectionManager connection() {
         return connection;
+    }
+
+    public OkHttpClient httpClient() {
+        return httpClient;
+    }
+
+    public OkHttpClient authedHttpClient() {
+        return authedHttpClient;
     }
 }
