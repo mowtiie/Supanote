a14dd72acc4463dc9ab5b12e88ea1a2a86ecf2e4
mowtiie <mowtiie.dev@gmail.com>
2026-07-07 23:58:40 +0800

    Added logic for about activity

--- app/src/main/java/com/mowtiie/supanote/ui/settings/AboutActivity.java
+++ app/src/main/java/com/mowtiie/supanote/ui/settings/AboutActivity.java
@@ -1,6 +1,10 @@
 package com.mowtiie.supanote.ui.settings;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -8,10 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.mowtiie.supanote.R;
 import com.mowtiie.supanote.databinding.ActivityAboutBinding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -44,9 +55,64 @@ public class AboutActivity extends AppCompatActivity {
 
     public static class AboutFragment extends PreferenceFragmentCompat {
 
+        private Preference appVersion;
+        private Preference appLicense;
+
+        private int easterEggCounter;
+
         @Override
         public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
             setPreferencesFromResource(R.xml.preferences_about, rootKey);
+
+            appVersion = findPreference("app_version");
+            appLicense = findPreference("app_license");
+
+            try {
+                PackageManager packageManager = requireContext().getPackageManager();
+                PackageInfo packageInfo = packageManager.getPackageInfo(requireContext().getPackageName(), 0);
+                appVersion.setSummary(packageInfo.versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            appVersion.setOnPreferenceClickListener(preference -> {
+                easterEggCounter++;
+                if (easterEggCounter == 7) {
+                    String easterEggMessage = getString(R.string.app_easter_egg);
+                    Toast.makeText(requireContext(), easterEggMessage, Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            });
+
+            appLicense.setOnPreferenceClickListener(preference -> {
+                showLicenseDialog();
+                return true;
+            });
+        }
+
+        private void showLicenseDialog() {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.preferences_app_license)
+                    .setIcon(R.drawable.ic_license)
+                    .setMessage(readLicenseFromAssets())
+                    .setPositiveButton(R.string.button_close, null)
+                    .show();
+        }
+
+        private String readLicenseFromAssets() {
+            StringBuilder stringBuilder = new StringBuilder();
+            AssetManager assetManager = requireContext().getAssets();
+
+            try (InputStream inputStream = assetManager.open("license.txt")) {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+            } catch (IOException e) {
+                Toast.makeText(requireContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            return stringBuilder.toString();
         }
     }
 }
